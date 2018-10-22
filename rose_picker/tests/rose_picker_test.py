3bc987951ede5068cfa39941afe4e5a1bd215bf0
rickywong <enquiries@metoffice.gov.uk>
2018-10-22 12:56:15 +0000

    ticket:1533: Update rose picker license headers
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/branches/dev/rickywong/r15753_MetaConfigurator/GPL@15961 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- rose_picker/tests/rose_picker_test.py
+++ rose_picker/tests/rose_picker_test.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 ##############################################################################
-# Copyright (c) 2017,  Met Office, on behalf of HMSO and Queen's Printer
-# For further details please refer to the file GPL/LICENCE which you
-# should have received as part of this distribution.
+# (C) British Crown Copyright 2018 Met Office.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rose. If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
 
 import unittest
